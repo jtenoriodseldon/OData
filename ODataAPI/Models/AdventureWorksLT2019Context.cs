dd11a3e0f9b08df3b5dad634f04bbc6d4a55d2ad
Juan M. Tenorio H. <j_tenorio@outlook.com>
2024-11-20 20:40:42 -0800

    Removed connection string from code and it is now only in appsettings

--- ODataAPI/Models/AdventureWorksLT2019Context.cs
+++ ODataAPI/Models/AdventureWorksLT2019Context.cs
@@ -37,9 +37,6 @@ public partial class AdventureWorksLT2019Context : DbContext
 
     public virtual DbSet<SalesOrderHeader> SalesOrderHeaders { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=AdventureWorksLT2019;Integrated Security=True;Encrypt=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
